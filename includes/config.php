578a841622eb2498da2836f0b9ce8e0c0641c10a
Newanga Wickramasinghe <newanga@live.com>
2020-01-06 16:46:25 +0530

    New connection strings

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,8 @@
 
 	$timezone = date_default_timezone_set("Asia/Colombo");
 
-	$con = mysqli_connect("localhost:3307", "root", "", "your cloud music player");
+	//$con = mysqli_connect("User@musicplayerdata", "User", "Demo@@123", "your cloud music player");
+	$con = mysqli_connect("localhost", "root", "", "your cloud music player");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
