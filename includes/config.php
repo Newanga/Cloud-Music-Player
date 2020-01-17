bddeb0df31c14197cf45e091d3f8f566a486cced
Newanga Wickramasinghe <newanga@live.com>
2020-01-17 16:39:34 +0530

    Bug fix

--- includes/config.php
+++ includes/config.php
@@ -4,8 +4,8 @@
 
 	$timezone = date_default_timezone_set("Asia/Colombo");
 
-	//$con = mysqli_connect("User@musicplayerdata", "User", "Demo@@123", "your cloud music player");
-	$con = mysqli_connect("localhost", "root", "", "your cloud music player");
+	$con = mysqli_connect("musicplayerdata.mysql.database.azure.com", "User@musicplayerdata", "Demo@@123", "your cloud music player");
+	//$con = mysqli_connect("localhost", "root", "", "your cloud music player");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
